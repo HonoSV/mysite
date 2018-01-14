04a56759eeee01b843130f97b88581d1443a4f4d
HonoSV <thbb268@live.com>
2018-01-14 23:24:15 +0800

    2

---
+++ polls/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from . import views
+
+app_name = '[polls]'  # 此处不加会报错'Specifying a namespace in include() without providing an app_name
+urlpatterns = [
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+]
